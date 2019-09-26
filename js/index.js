bade4662c0a1a5bf1b606be3312a9cfd0fe6be9e
xuyue <921859993@qq.com>
2019-09-26 15:19:07 +0800

    111

--- js/index.js
+++ js/index.js
@@ -1,13 +1,15 @@
-(function (width) {
-    var doc = width.document,
+(function (window) {
+    var doc = window.document,
       element = doc.documentElement,
       i = 640,
       d = i / 100,
-      o = "orientationchange" in width ? "orientationchange" : "resize",
+      o = "orientationchange" in window ? "orientationchange" : "resize",
       a = function () {
         var width = element.clientWidth || 320;
+        console.log(width);
         width > 640 && (width = 640);
         element.style.fontSize = width / d + "px"
       };
-    doc.addEventListener && (width.addEventListener(o, a, !1), doc.addEventListener("DOMContentLoaded", a, !1))
+      console.log(window.document);
+    doc.addEventListener && (window.addEventListener(o, a, !1), doc.addEventListener("DOMContentLoaded", a, !1))
   })(window);
